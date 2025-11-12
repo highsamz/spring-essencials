bf9a3069e8d993ad1b5f16c7eedd51ba38e73d8f
samue <samuel.cruz_ntconsult@prestador.globo>
2025-11-11 23:47:38 -0300

    primeiro commit

---
+++ src/main/java/br/com/springboot_essencials/SpringbootEssencialsApplication.java
@@ -0,0 +1,13 @@
+package br.com.springboot_essencials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEssencialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEssencialsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/springboot_essencials/SpringbootEssencialsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.springboot_essencials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEssencialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
